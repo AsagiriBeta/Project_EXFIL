0a3ac86fd45f192e58d6f575191c6bff4a345e83
Kingcq <404291187@qq.com>
2025-11-25 10:48:57 +0800

    Initialize project with Minecraft version 1.21.6

---
+++ src/main/java/org/nmo/project_exfil/ProjectEXFILPlugin.java
@@ -0,0 +1,17 @@
+package org.nmo.project_exfil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProjectEXFILPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
