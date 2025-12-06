4bcbdd74e68b59b44c5fec63bfafcaa17c74e9e2
Kingcq <404291187@qq.com>
2025-12-06 19:41:42 +0800

    feat: almost complete ChestUI

--- src/main/java/org/nmo/project_exfil/ChestUIHelper/Components/ChestUIComponent.java
+++ src/main/java/org/nmo/project_exfil/ChestUIHelper/Components/ChestUIComponent.java
@@ -4,7 +4,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Base class for components of ChestUI
+ */
 public abstract class ChestUIComponent {
+    /**
+     * Return the actual position in inventory
+     * @param row The required row
+     * @param col The required col
+     * @return The actual index in inventory
+     */
+    private static Integer toPosition(int row, int col) {
+        return row * 9 + col;
+    }
+
     /**
      * Returns the number of rows the component takes up
      * @return The number of rows the component takes up
@@ -28,6 +41,16 @@ public abstract class ChestUIComponent {
      * @param col The column to render onto
      */
     void render(@NotNull Inventory inventory, @NotNull Integer row, @NotNull Integer col) {
+        // Do nothing by default
+    }
+
+    /**
+     * Updates the component
+     * @param inventory The inventory to render onto
+     * @param row The row to render onto
+     * @param col The column to render onto
+     */
+    void update(@NotNull Inventory inventory, @NotNull Integer row, @NotNull Integer col) {
         // Do nothing by default
     }
 
--- src/main/java/org/nmo/project_exfil/ProjectEXFILPlugin.java
+++ src/main/java/org/nmo/project_exfil/ProjectEXFILPlugin.java
@@ -1,12 +1,16 @@
 package org.nmo.project_exfil;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 import org.nmo.project_exfil.ChestUIHelper.ChestUIHelper;
 
 public final class ProjectEXFILPlugin extends JavaPlugin {
     private static ProjectEXFILPlugin plugin = null;
+
     private static ChestUIHelper chestUIHelper = null;
+
+    private static YamlConfiguration config = null;
 
     /**
      * Get the plugin instance
@@ -14,6 +18,14 @@ public final class ProjectEXFILPlugin extends JavaPlugin {
      */
     public static @Nullable ProjectEXFILPlugin getPlugin() {
         return plugin;
+    }
+
+    /**
+     * Get the default plugin config
+     * @return The plugin config
+     */
+    public static @Nullable YamlConfiguration getDefaultConfig() {
+        return config;
     }
 
     /**
@@ -30,6 +42,9 @@ public final class ProjectEXFILPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+
+        this.saveDefaultConfig();
+        config = (YamlConfiguration) this.getConfig();
 
         // Initialize ChestUIHelper
         chestUIHelper = new ChestUIHelper();
