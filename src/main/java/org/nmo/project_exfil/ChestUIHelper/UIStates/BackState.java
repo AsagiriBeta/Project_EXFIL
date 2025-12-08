b3bc1c1dbb51a50d16d4e15a779b73bb2aff59e1
AsagiriBeta <2839636492@qq.com>
2025-12-08 19:05:17 +0800

    Added dependencies:Parties,Adventure Added support for multi languages,Stash

---
+++ src/main/java/org/nmo/project_exfil/ChestUIHelper/UIStates/BackState.java
@@ -0,0 +1,8 @@
+package org.nmo.project_exfil.ChestUIHelper.UIStates;
+
+/**
+ * This state only stands for popping the current state in the state stack
+ */
+public class BackState extends ChestUIState {
+    BackState() {}
+}
--- src/main/java/org/nmo/project_exfil/listener/DeathListener.java
+++ src/main/java/org/nmo/project_exfil/listener/DeathListener.java
@@ -27,21 +27,13 @@ public class DeathListener implements Listener {
         Player player = event.getEntity();
         World world = player.getWorld();
 
-        // Check if player is in a game world (not lobby)
-        // Assuming lobby world is named "lobby"
         if (!world.getName().equalsIgnoreCase("lobby")) {
             // It's a raid world
-            event.deathMessage(null); // Hide default message
+            event.deathMessage(null);
             
-            // Send custom message
             plugin.getLanguageManager().send(player, "exfil.death_raid");
             plugin.getLanguageManager().send(player, "exfil.death_lost");
             
-            // Ensure drops happen (default behavior)
-            // But we might want to clear drops if we want "secure container" logic later
-            // For now, let it drop.
-            
-            // We can also announce to others in that world
             for (Player p : world.getPlayers()) {
                 p.sendMessage("§c☠ " + player.getName() + " was killed.");
             }
@@ -52,10 +44,7 @@ public class DeathListener implements Listener {
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         Player player = event.getPlayer();
         
-        // If they died in a raid, force them back to lobby
-        // We can check if the respawn location is NOT lobby, then force lobby
         if (!event.getRespawnLocation().getWorld().getName().equalsIgnoreCase("lobby")) {
-            // We need to find the lobby world
             World lobby = Bukkit.getWorld("lobby");
             if (lobby != null) {
                 event.setRespawnLocation(lobby.getSpawnLocation());
